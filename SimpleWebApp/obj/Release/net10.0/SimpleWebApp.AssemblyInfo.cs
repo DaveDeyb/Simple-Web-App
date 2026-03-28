dfac7c079bda0ae240b5b8695aab1d85ec33b38e
Dave <marin.283984@ortigas-cainta.sti.edu.ph>
2026-03-28 11:49:01 +0800

    Eerm

--- SimpleWebApp/obj/Release/net10.0/SimpleWebApp.AssemblyInfo.cs
+++ SimpleWebApp/obj/Release/net10.0/SimpleWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b389defd2fdcae801146d54f6260faf351e8eb1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb76162d1a26a946fff5a6141d8664bb25a42a3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
